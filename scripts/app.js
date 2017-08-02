c9a91aee88d17cadd14cf312b1e100e168510e1b
Rosando Desouza <desouza.rosando@gmail.com>
2017-08-02 23:53:20 +0530

    Add level 2 to flatlander store

--- scripts/app.js
+++ scripts/app.js
@@ -5,19 +5,49 @@
 		this.products = gems;
 	});
 	
+	app.controller("PanelController", function(){
+		this.tab = 1;
+		
+		this.selectTab = function(value){
+			this.tab = value;
+		};
+		
+		this.isSelected = function(value){
+			return this.tab === value;
+		};
+	});
+	
+	app.controller("GalleryController", function(){
+		this.current = 0;
+		
+		this.setCurrent = function(value){
+			this.current = value || 0;
+		};
+	});
+	
 	var gems = [
 		{
 			name: "Dodecahedron",
 			price: 2.95,
 			description: "First gem found",
 			canPurchase: true,//if this property is not present on the object then angular will treat it as a false value and not show it on the page
-			soldOut: false
+			soldOut: false,
+			images:[
+				"content/images/gem1.jpg",
+				"content/images/gem1a.jpg"
+			],
+			description: "some description on gem 1"
 		},
 		{
 			name: "Pentagonal Gem",
 			price: 5.95,
 			description: "Second gem found",
-			canPurchase: false
+			canPurchase: false,
+			images:[
+				"content/images/gem3.jpg",
+				"content/images/gem3a.jpg"
+			],
+			description: "some description on gem 2"
 		}
 	];
 })();
