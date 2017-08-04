8495e8be82a1a37d657dc0c4cd0f15469f13cc02
Rosando Desouza <desouza.rosando@gmail.com>
2017-08-05 01:02:25 +0530

    Added level 4

--- scripts/app.js
+++ scripts/app.js
@@ -5,17 +5,9 @@
 		this.products = gems;
 	});
 	
-	app.controller("PanelController", function(){
-		this.tab = 1;
+	//app.controller("PanelController", function(){
 		
-		this.selectTab = function(value){
-			this.tab = value;
-		};
-		
-		this.isSelected = function(value){
-			return this.tab === value;
-		};
-	});
+	//});
 	
 	app.controller("GalleryController", function(){
 		this.current = 0;
@@ -32,6 +24,39 @@
 			product.reviews.push(this.review);
 			this.review = {};
 		};
+	});
+	
+	app.directive("productTitle", function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-title.html'
+		};
+	});
+	
+	app.directive("productTitleAttr", function(){
+		return {
+			restrict: "A",
+			templateUrl: "product-title.html"
+		};
+	});
+	
+	app.directive("productPanel", function(){
+		return {
+			restrict: "E",
+			templateUrl: "product-panel.html",
+			controller: function(){
+				this.tab = 1;
+		
+				this.selectTab = function(value){
+					this.tab = value;
+				};
+				
+				this.isSelected = function(value){
+					return this.tab === value;
+				};
+			},
+			controllerAs: "panel"
+		}
 	});
 	
 	var gems = [
